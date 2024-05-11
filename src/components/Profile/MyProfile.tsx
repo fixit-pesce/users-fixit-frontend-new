fd338e0d3739acf0d338fc502012808c2d3a7247
Pranav K Bhaskar <pkb19032002@gmail.com>
2024-05-11 10:14:25 +0530

    Fix: MyProfile loading

--- src/components/Profile/MyProfile.tsx
+++ src/components/Profile/MyProfile.tsx
@@ -23,15 +23,15 @@ export default function MyProfile() {
     queryFn: () => getUser({ username }),
   })
 
-  const [email, setEmail] = useState(data?.email ?? "")
-  const [firstName, setFirstName] = useState(data?.first_name ?? "")
-  const [lastName, setLastName] = useState(data?.last_name ?? "")
+  const [email, setEmail] = useState(data?.email as string)
+  const [firstName, setFirstName] = useState(data?.first_name as string)
+  const [lastName, setLastName] = useState(data?.last_name as string)
   // const [phoneNo, setPhoneNo] = useState(data?.phone_no ?? "")
 
   useEffect(() => {
-    setEmail(data?.email ?? "")
-    setFirstName(data?.first_name ?? "")
-    setLastName(data?.last_name ?? "")
+    setEmail(data?.email as string)
+    setFirstName(data?.first_name as string)
+    setLastName(data?.last_name as string)
     // setPhoneNo(data?.phone_no ?? "")
   }, [data])
 
@@ -79,68 +79,70 @@ export default function MyProfile() {
 
   return (
     <Box w="100%">
-      <Flex
-        justifyContent="center"
-        alignItems="center"
-        bg="white"
-        p="4"
-        w="50%"
-        mx="auto"
-        mt="4"
-        flexDir="column"
-        gap="4"
-        boxShadow="md"
-        rounded="md"
-      >
-        <Heading mt="6" color="primary.900">
-          Profile Details - {data?.username}
-        </Heading>
-        <FormControl>
-          <FormLabel>Email</FormLabel>
-          <EditableTextInput
-            defaultValue={data?.email ?? ""}
-            type="input"
-            setText={setEmail}
-          />
-        </FormControl>
-        <FormControl>
-          <FormLabel>First Name</FormLabel>
-          <EditableTextInput
-            defaultValue={data?.first_name ?? ""}
-            type="input"
-            setText={setFirstName}
-          />
-        </FormControl>
-        <FormControl>
-          <FormLabel>Last Name</FormLabel>
-          <EditableTextInput
-            defaultValue={data?.last_name ?? ""}
-            type="input"
-            setText={setLastName}
-          />
-        </FormControl>
-        {/* <FormControl>
+      {data && (
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          bg="white"
+          p="4"
+          w="50%"
+          mx="auto"
+          mt="4"
+          flexDir="column"
+          gap="4"
+          boxShadow="md"
+          rounded="md"
+        >
+          <Heading mt="6" color="primary.900">
+            Profile Details - {data?.username}
+          </Heading>
+          <FormControl>
+            <FormLabel>Email</FormLabel>
+            <EditableTextInput
+              defaultValue={data.email}
+              type="input"
+              setText={setEmail}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel>First Name</FormLabel>
+            <EditableTextInput
+              defaultValue={data?.first_name}
+              type="input"
+              setText={setFirstName}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Last Name</FormLabel>
+            <EditableTextInput
+              defaultValue={data?.last_name}
+              type="input"
+              setText={setLastName}
+            />
+          </FormControl>
+          {/* <FormControl>
           <FormLabel>Phone number</FormLabel>
           <EditableTextInput defaultValue = {data?.phone_no ?? ""} type = "input" setText = {setPhoneNo}/>
         </FormControl> */}
-        <Flex gap="4">
-          <Button colorScheme="green" onClick={onOpen}>
-            Change Password
-          </Button>
-          <Button
-            colorScheme="blue"
-            onClick={handleSubmit}
-            isLoading={isSaveLoading}
-          >
-            Save
-          </Button>
+          <Flex gap="4">
+            <Button colorScheme="green" onClick={onOpen}>
+              Change Password
+            </Button>
+            <Button
+              colorScheme="blue"
+              onClick={handleSubmit}
+              isLoading={isSaveLoading}
+            >
+              Save
+            </Button>
+          </Flex>
+          <ChangePasswordModal
+            isOpen={isOpen}
+            onClose={onClose}
+            username={username}
+          />
         </Flex>
-        <ChangePasswordModal
-          isOpen={isOpen}
-          onClose={onClose}
-          username={username}
-        />
-      </Flex>
+      )}
     </Box>
   )
 }
