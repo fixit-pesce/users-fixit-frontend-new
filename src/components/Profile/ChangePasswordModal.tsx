f0a766eac909dbca7807184b758b5cc7ae98d553
Pranav K Bhaskar <pkb19032002@gmail.com>
2024-05-11 20:20:49 +0530

    Fix: Minor fixes

--- src/components/Profile/ChangePasswordModal.tsx
+++ src/components/Profile/ChangePasswordModal.tsx
@@ -1,8 +1,21 @@
-import { Button, FormControl, FormLabel, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useToast } from '@chakra-ui/react'
-import { useMutation } from '@tanstack/react-query'
-import { useState } from 'react'
-import { updatePassword } from '../../api/userApi'
-import { AxiosError } from 'axios'
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useToast,
+} from "@chakra-ui/react"
+import { useMutation } from "@tanstack/react-query"
+import { useState } from "react"
+import { updatePassword } from "../../api/userApi"
+import { AxiosError } from "axios"
 
 interface ChangePasswordModalProps {
   isOpen: boolean
@@ -10,12 +23,17 @@ interface ChangePasswordModalProps {
   username: string
 }
 
-export default function ChangePasswordModal({isOpen, onClose, username}: ChangePasswordModalProps) {
+export default function ChangePasswordModal({
+  isOpen,
+  onClose,
+  username,
+}: ChangePasswordModalProps) {
   const [currentPassword, setCurrentPassword] = useState("")
   const [newPassword, setNewPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
 
   const toast = useToast()
-  
+
   const mutation = useMutation({
     mutationFn: updatePassword,
     onSuccess: () => {
@@ -30,23 +48,40 @@ export default function ChangePasswordModal({isOpen, onClose, username}: ChangeP
     onError: (res: AxiosError) => {
       toast({
         position: "top",
-        title: res.response?.data ? `Error: ${Object.entries(res.response?.data)[0][1]}` : `Error: ${res.message}`,
-        status: 'error',
+        title: res.response?.data
+          ? `Error: ${Object.entries(res.response?.data)[0][1]}`
+          : `Error: ${res.message}`,
+        status: "error",
         duration: 3000,
         isClosable: true,
       })
-    }
+    },
   })
 
   const handleSubmit = () => {
-    mutation.mutate({username, current_password: currentPassword, new_password: newPassword})
+    if (newPassword !== confirmPassword) {
+      toast({
+        position: "top",
+        title: "Passwords do not match",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      })
+      return
+    }
+
+    mutation.mutate({
+      username,
+      current_password: currentPassword,
+      new_password: newPassword,
+    })
     setCurrentPassword("")
     setNewPassword("")
     onClose()
   }
 
   return (
-    <Modal isOpen = {isOpen} onClose={onClose}>
+    <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>Change Password</ModalHeader>
@@ -54,16 +89,39 @@ export default function ChangePasswordModal({isOpen, onClose, username}: ChangeP
         <ModalBody>
           <FormControl>
             <FormLabel htmlFor="current-password">Current Password</FormLabel>
-            <Input id="current-password" value = {currentPassword} onChange={(e) => setCurrentPassword(e.target.value)}/>
+            <Input
+              id="current-password"
+              type="password"
+              value={currentPassword}
+              onChange={(e) => setCurrentPassword(e.target.value)}
+            />
           </FormControl>
           <FormControl>
             <FormLabel htmlFor="new-password">New Password</FormLabel>
-            <Input id="new-password" value = {newPassword} onChange={(e) => setNewPassword(e.target.value)}/>
+            <Input
+              id="new-password"
+              type="password"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="new-password">Confirm Password</FormLabel>
+            <Input
+              id="confirm-password"
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
           </FormControl>
         </ModalBody>
-        <ModalFooter justifyContent="center" gap = "4">
-          <Button onClick={handleSubmit} colorScheme="blue">Submit</Button>
-          <Button onClick={onClose} colorScheme='red'>Cancel</Button>
+        <ModalFooter justifyContent="center" gap="4">
+          <Button onClick={handleSubmit} colorScheme="blue">
+            Submit
+          </Button>
+          <Button onClick={onClose} colorScheme="red">
+            Cancel
+          </Button>
         </ModalFooter>
       </ModalContent>
     </Modal>
